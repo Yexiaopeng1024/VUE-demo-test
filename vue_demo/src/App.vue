d369f24d10e81181e2279c09ce7749e872c5862c
叶小鹏 <helloye1024@foxmail.comm>
2021-03-02 23:54:42 +0800

    VUEdemo

---
+++ vue_demo/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+        <Add :addComment="addComment"/>
+        <List :comments="comments"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Add from './components/Add'
+import List from "./components/List";
+export default {
+    data () {
+        return {
+            comments:[
+                {name:"Bob",
+                content:'Vue is  Good'
+                },
+                {name:"Dog",
+                content:'Vue So easy!'
+                },
+                {name:"Cat",
+                content:'Vue I like'
+                }
+                ]
+        }
+    },
+    methods: {
+        addComment(comment){
+            this.comments.unshift(comment)
+        }
+    },
+components: {
+    Add,
+    List 
+}
+}
+</script>
+
+<style>
+
+</style>
